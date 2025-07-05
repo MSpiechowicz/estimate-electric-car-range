9f2be1f36ed3eacf8f5b23c7156e0f1aa3c22d15
Maciej Spiechowicz <spm1fr@bosch.com>
2025-07-05 23:21:31 +0200

    Refactor AppHeader, AppHome, and AppNotificationBar components for improved layout and responsiveness; adjust icon sizes and spacing for better visual consistency across the application.

--- src/lib/components/AppHeader.svelte
+++ src/lib/components/AppHeader.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class="flex flex-row items-center justify-center gap-2">
-  <IconPlug customClass="!w-10 !h-auto" customColor="black" />
+  <IconPlug customClass="!w-10 !h-auto hidden md:block" customColor="black" />
   <h1 class="text-4xl font-bold ">Electric Car Range Calculator</h1>
 </div>
--- src/lib/components/AppHome.svelte
+++ src/lib/components/AppHome.svelte
@@ -15,7 +15,7 @@
   let showAdvanced = $state(false);
 </script>
 
-<div class={`flex justify-center items-center min-h-screen ${showAdvanced ? "my-10" : ""}`}>
+<div class={`p-4 flex justify-center items-center min-h-screen ${showAdvanced ? "my-10" : ""}`}>
   <div class="max-w-screen-sm w-full bg-white p-6 rounded-lg shadow-md">
     <div class="flex flex-col items-start justify-center gap-2">
       <AppHeader />
@@ -27,9 +27,9 @@
         <AppAdvancedInputs bind:showAdvanced />
       </div>
 
-      <div class="flex flex-row items-start justify-center gap-2">
+      <div class="flex flex-col md:flex-row items-start justify-center gap-0 md:gap-2">
         <button
-          class="my-4 px-4 py-2 bg-button-primary hover:bg-button-hover text-white rounded self-stretch flex items-center justify-center gap-2 w-54"
+          class="my-2 md:my-4 px-4 py-2 bg-button-primary hover:bg-button-hover text-white rounded self-stretch flex items-center justify-center gap-2 w-54"
           onclick={() => {
             const { rangeKm, rangeMi } = calculateRange();
             carStore.range = rangeKm;
@@ -40,7 +40,7 @@
           Calculate Range
         </button>
         <button
-          class="my-4 px-4 py-2 bg-trasparent border-2 border-secondary text-secondary hover:text-white hover:bg-secondary rounded self-stretch flex items-center justify-center gap-2"
+          class="my-2 md:my-4 px-4 py-2 bg-trasparent border-2 border-secondary text-secondary hover:text-white hover:bg-secondary rounded self-stretch flex items-center justify-center gap-2"
           onclick={() => (showAdvanced = !showAdvanced)}
         >
           {#if showAdvanced}
